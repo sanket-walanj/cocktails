c2209d0856b334caf5b64634ffabdb75c7e3b83b
sanket-walanj <code.sanky@gmail.com>
2022-03-29 12:11:54 +0530

    remove console log

--- app.js
+++ app.js
@@ -6,4 +6,3 @@ const URL = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=a';
 window.addEventListener('DOMContentLoaded', () => {
   presentDrinks(URL);
 });
-// console.log('Continuous Deployment');
